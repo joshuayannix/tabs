7bdce8df7f037480d634a7ea90cee2052fa61ad0
Joshua Yannix <joshuayannix@gmail.com>
2020-08-07 21:16:51 -0500

    complete hide and show functionality

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ about.addEventListener('click', e => {
   const id = e.target.dataset.id;
   console.log(id)
   if(id){
+    //remove active from other buttons
     btns.forEach(btn => {
       btn.classList.remove('active');
       e.target.classList.add('active');
     })
-
+    // hide other articles
+    articles.forEach(art => {
+      art.classList.remove('active')      
+    })
+    let element = document.getElementById(id)
+    console.log(element)
+    element.classList.add('active')
   }
 })
